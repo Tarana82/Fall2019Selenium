dc5083761b95a32f1550dfb5f559cabd5f2533c9
Tarana82 <tahmadova1982@gmail.com>
2020-03-03 10:23:56 -0600

    created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class JumpToTheNewWindow {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
@@ -19,5 +21,43 @@ public class JumpToTheNewWindow {
         String windowHandle = driver.getWindowHandle();
 
         System.out.println(windowHandle);
+
+        //getWindowHandles() - returns id's of all currently opened windows
+        //Set - doesn't allow duplicates
+        Set<String> windowHandles = driver.getWindowHandles();
+        System.out.println(windowHandles);
+        System.out.println("BEFORE SWITCH :" + driver.getCurrentUrl());
+        //since we have all windows
+        //and we know id of original window
+        //we can say switch to something that is not equals to old window id
+        for (String windowId : windowHandles) {
+            //if it's not an old window, then switch
+            if (!windowId.equals(windowHandle)) {
+                //to jump to the new window
+                driver.switchTo().window(windowId);
+            }
+        }
+        System.out.println("AFTER SWITCH :" + driver.getCurrentUrl());
+        //driver.close();
+        driver.quit();
+
+        /**
+         *
+         * This method helps to switch in between windows based on page title
+         * @param pageTitle
+         * @param driver
+         */
+    }
+
+    public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver) {
+        Set<String> windows = driver.getWindowHandles();
+        for (String window : windows) {
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(pageTitle)) {
+                break;
+            }
+
+        }
+
     }
 }
