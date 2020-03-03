35fe5fb5cd3caab9438dc4b23a905d329033971b
Tarana82 <tahmadova1982@gmail.com>
2020-03-02 23:55:39 -0600

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -16,6 +16,19 @@ public class BasicNavigation {
         //ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com");//to open a website
         Thread.sleep(3000);//for demo, wait 3 seconds
+        //method that return page title
+        //you can also see it as tab name, in the browser
+        String title = driver.getTitle(); //returns <title>Some title</title> text
+        String expectedTitle = "Google";
+
+        System.out.println("Title is..." + title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+    }else {
+            System.out.println("TEST FAILED!");
+        }
+        //must be at the end
         driver.close();//to cloe browser
     }
 }
