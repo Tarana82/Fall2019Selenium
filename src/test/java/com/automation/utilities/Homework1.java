101bfd5f492892e67742e281f0be9ce56278088f
Tarana82 <tahmadova1982@gmail.com>
2020-03-04 13:17:10 -0600

    created basic navigation class

---
+++ src/test/java/com/automation/utilities/Homework1.java
@@ -0,0 +1,12 @@
+package com.automation.utilities;
+import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Homework1 {
+
+
+}
+
