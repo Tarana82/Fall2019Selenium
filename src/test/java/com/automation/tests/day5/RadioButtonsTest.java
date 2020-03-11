2ec77c2094096c47e1393a2a780a98a7a4de78aa
Tarana82 <tahmadova1982@gmail.com>
2020-03-11 11:28:20 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/RadioButtonsTest.java
+++ src/test/java/com/automation/tests/day5/RadioButtonsTest.java
@@ -2,32 +2,45 @@ package com.automation.tests.day5;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RadioButtonsTest {
-    public static void main(String[] args) {
-        WebDriver driver = DriverFactory.createDriver("chrome");
-        driver.get("http://practce.cybertekschool.com/radio_buttons");
-        BrowserUtils.wait(2);
 
+    public static void main(String[] args) {
+//        if DriverFactory doesn't work, use this:
+//        WebDriverManager.chromedriver().version("79").setup();
+//        WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        BrowserUtils.wait(4);
         //<input type="radio" id="black" name="color">
         WebElement blackButton = driver.findElement(By.id("black"));
 
-        blackButton.click();
+        //if visible and eligible to click
+        if(blackButton.isDisplayed() && blackButton.isEnabled()){
+            System.out.println("CLICKED ON BLACK BUTTON");
+            blackButton.click();
+        }else {
+            System.out.println("FAILED TO CLICK ON BLACK BUTTON");
+        }
+
+        BrowserUtils.wait(2);
 
         //how do we verify that button clicked
         //returns true, if button clicked
-        if (blackButton.isSelected()) {
+        if(blackButton.isSelected()){
             System.out.println("TEST PASSED!");
-        } else {
+        }else {
             System.out.println("TEST FAILED");
-
-
-            driver.quit();
-
-
         }
+
+
+
+        driver.quit();
     }
 }
