56c679ba1caa32a994d39a37b848c61d139040a4
Tarana82 <tahmadova1982@gmail.com>
2020-03-11 11:24:03 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/RadioButtonsTest.java
+++ src/test/java/com/automation/tests/day5/RadioButtonsTest.java
@@ -2,7 +2,9 @@ package com.automation.tests.day5;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class RadioButtonsTest {
     public static void main(String[] args) {
@@ -10,6 +12,22 @@ public class RadioButtonsTest {
         driver.get("http://practce.cybertekschool.com/radio_buttons");
         BrowserUtils.wait(2);
 
+        //<input type="radio" id="black" name="color">
+        WebElement blackButton = driver.findElement(By.id("black"));
 
+        blackButton.click();
+
+        //how do we verify that button clicked
+        //returns true, if button clicked
+        if (blackButton.isSelected()) {
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED");
+
+
+            driver.quit();
+
+
+        }
     }
 }
