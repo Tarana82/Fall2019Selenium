233ab45f856e0f726eb5eebbe2925fd59458836b
Tarana82 <tahmadova1982@gmail.com>
2020-03-27 14:44:01 -0500

    day 8 BasicTestNGTests

--- src/test/java/com/automation/utilities/BrowserUtils.java
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -1,5 +1,8 @@
 package com.automation.utilities;
+import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
 public class BrowserUtils {
     /**
      * Pause test for some time
@@ -11,5 +14,17 @@ public class BrowserUtils {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    /**
+     *
+     * @param elements represents collection of WebElements
+     * @return collection of strings
+     */
+    public static List<String> getTextFromWebElements(List<WebElement> elements) {
+        List<String> textValues = new ArrayList<>();
+        for (WebElement element : elements) {
+            textValues.add(element.getText());
+        }
+        return textValues;
     }
 }
