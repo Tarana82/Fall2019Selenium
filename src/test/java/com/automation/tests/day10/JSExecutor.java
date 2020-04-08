b2edb5fae48ed14e13e170dd0fe32bfb57d6b08c
Tarana82 <tahmadova1982@gmail.com>
2020-04-07 20:58:55 -0500

    March22

--- src/test/java/com/automation/tests/day10/JSExecutor.java
+++ src/test/java/com/automation/tests/day10/JSExecutor.java
@@ -1,11 +1,8 @@
 package com.automation.tests.day10;
 
 import com.automation.utilities.BrowserUtils;
-import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
